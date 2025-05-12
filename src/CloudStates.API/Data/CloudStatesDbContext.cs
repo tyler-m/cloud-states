3d0cfb8e82505d4d9278700470a5ae3f71b712e5
Tyler <tylermartel@outlook.com>
2025-05-12 13:11:21 -0400

    add postgres db context to api and postgres service to development environment

---
+++ src/CloudStates.API/Data/CloudStatesDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CloudStates.API.Data
+{
+    public class CloudStatesDbContext(DbContextOptions<CloudStatesDbContext> options) : DbContext(options)
+    {
+
+    }
+}
--- src/CloudStates.API/Program.cs
+++ src/CloudStates.API/Program.cs
@@ -1,15 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using CloudStates.API.Data;
 
-namespace CloudStates
+namespace CloudStates.API
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            ConfigurationManager config = builder.Configuration;
+
+            // postgres
+            string? postgresString = config.GetConnectionString("Postgres")
+                ?? throw new InvalidOperationException("Postgres connection string is missing.");
+            builder.Services.AddDbContext<CloudStatesDbContext>(options => options.UseNpgsql(postgresString));
+
             WebApplication app = builder.Build();
-
             app.MapGet("/", () => "Cloud States");
-
             app.Run();
         }
     }
