abccfc9f0a95939fd9568774b84a77e455f81e82
Tyler <tylermartel@outlook.com>
2025-05-15 17:50:45 -0400

    add remove method to pre-signed url repository

--- src/CloudStates.API/Repositories/IPreSignedUrlRepository.cs
+++ src/CloudStates.API/Repositories/IPreSignedUrlRepository.cs
@@ -5,5 +5,6 @@ namespace CloudStates.API.Repositories
     public interface IPreSignedUrlRepository
     {
         Task<bool> AddAsync(PreSignedUrl preSignedUrl);
+        Task<bool> RemoveAsync(string fileKey);
     }
 }
--- src/CloudStates.API/Repositories/PreSignedUrlRepository.cs
+++ src/CloudStates.API/Repositories/PreSignedUrlRepository.cs
@@ -10,5 +10,18 @@ namespace CloudStates.API.Repositories
             await _db.PreSignedUrls.AddAsync(preSignedUrl);
             return await _db.SaveChangesAsync() > 0;
         }
+
+        public async Task<bool> RemoveAsync(string fileKey)
+        {
+            PreSignedUrl? presignedUrl = _db.PreSignedUrls.Where(url => url.Key == fileKey).FirstOrDefault();
+
+            if (presignedUrl != null)
+            {
+                _db.Remove(presignedUrl);
+                return await _db.SaveChangesAsync() > 0;
+            }
+
+            return false;
+        }
     }
 }
