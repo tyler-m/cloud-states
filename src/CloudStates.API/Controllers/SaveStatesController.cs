4e20131505b3d30657d71c31fa00e21dc14de659
Tyler <tylermartel@outlook.com>
2025-05-15 15:49:07 -0400

    add save states controller and an authorized route for acquiring a save state upload url

---
+++ src/CloudStates.API/Controllers/SaveStatesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using CloudStates.API.Dtos;
+using CloudStates.API.Services;
+
+namespace CloudStates.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SaveStatesController(ISaveStateService _saveStateService) : ControllerBase
+    {
+        [Authorize]
+        [HttpGet("upload-url")]
+        [ProducesResponseType(typeof(SaveStateUploadUrlResponse), StatusCodes.Status200OK)]
+        public async Task<ActionResult<SaveStateUploadUrlResponse>> GetUploadUrlAsync()
+        {
+            return Ok(await _saveStateService.GetUploadUrlAsync());
+        }
+    }
+}
