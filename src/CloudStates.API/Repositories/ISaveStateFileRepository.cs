d48edd2bac14bf29d659e0f14b84a4e1b3baa4aa
Tyler <tylermartel@outlook.com>
2025-05-16 17:09:07 -0400

    add method to get a save state download url given a file key

--- src/CloudStates.API/Repositories/ISaveStateFileRepository.cs
+++ src/CloudStates.API/Repositories/ISaveStateFileRepository.cs
@@ -4,6 +4,7 @@ namespace CloudStates.API.Repositories
     public interface ISaveStateFileRepository
     {
         Task<string> GetUploadUrlAsync(string fileKey, DateTime expiresAt);
+        Task<string> GetDownloadUrlAsync(string fileKey, DateTime expiresAt);
         Task<bool> ExistsAsync(string fileKey);
     }
 }
--- src/CloudStates.API/Repositories/SaveStateFileRepository.cs
+++ src/CloudStates.API/Repositories/SaveStateFileRepository.cs
@@ -4,7 +4,10 @@ using CloudStates.API.Options;
 
 namespace CloudStates.API.Repositories
 {
-    internal class SaveStateFileRepository(AmazonS3Client _s3, S3Options _options) : ISaveStateFileRepository
+    internal class SaveStateFileRepository(
+        AmazonS3Client _s3,
+        S3Options _options
+        ) : ISaveStateFileRepository
     {
         public async Task<string> GetUploadUrlAsync(string fileKey, DateTime expiresAt)
         {
@@ -16,6 +19,20 @@ namespace CloudStates.API.Repositories
                 Expires = expiresAt,
                 Verb = HttpVerb.PUT,
                 ContentType = "application/octet-stream",
+            };
+
+            return await _s3.GetPreSignedURLAsync(request);
+        }
+
+        public async Task<string> GetDownloadUrlAsync(string fileKey, DateTime expiresAt)
+        {
+            GetPreSignedUrlRequest request = new()
+            {
+                Protocol = (Protocol)_options.Protocol,
+                BucketName = _options.SaveStatesBucket,
+                Key = fileKey,
+                Expires = expiresAt,
+                Verb = HttpVerb.GET
             };
 
             return await _s3.GetPreSignedURLAsync(request);
